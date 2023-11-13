2868e6f8c91de5626788a9e7a0b3616d29b1f98f
Jakub Mierzwa <kontakt.jakubmierzwa@gmail.com>
2023-11-13 18:49:26 +0100

    [#8] Add unit tests for CaesarCipher and XORCipher classes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "Cipher.hpp"
+#include "XORCipher.hpp"
 
 int main()
 {
@@ -12,8 +13,9 @@ int main()
     std::cout << "Data Encryption and Decryption System" << std::endl;
     std::cout << "Enter the data to encrypt: ";
     getline(std::cin, input);
+    XORCipher cipher("123");
 
-    std::cout << "Encrypted Data: " << encrypted << std::endl;
+    std::cout << "Encrypted Data: " << cipher.encrypt(input) << std::endl;
 
     std::cout << "Decrypted Data: " << decrypted << std::endl;
 
--- test/Data_Encryption_Tests.cpp
+++ test/Data_Encryption_Tests.cpp
@@ -1,3 +1,36 @@
 #include <gtest/gtest.h>
 #include "..\CaesarCipher.hpp"
 #include "..\XORCipher.hpp"
+
+TEST(CaesarCipherTest, Encrypt)
+{
+    CaesarCipher cipher(3);
+    std::string originalText = "hello";
+    std::string encryptedText = cipher.encrypt(originalText);
+    ASSERT_EQ("khoor", encryptedText);
+}
+
+TEST(CaesarCipherTest, Decrypt)
+{
+    CaesarCipher cipher(3);
+    std::string encryptedText = "khoor";
+    std::string decryptedText = cipher.decrypt(encryptedText);
+    ASSERT_EQ("hello", decryptedText);
+}
+
+TEST(XORCipherTest, Encrypt) 
+{
+    XORCipher cipher("123");
+    std::string originalText = "hello";
+    std::string encryptedText = cipher.encrypt(originalText);
+    std::string expectedEncryption = "YW_]]";
+    ASSERT_EQ(expectedEncryption, encryptedText);
+}
+
+TEST(XORCipherTest, Decrypt) 
+{
+    XORCipher cipher("123");
+    std::string encryptedText = "YW_]]";
+    std::string decryptedText = cipher.decrypt(encryptedText);
+    ASSERT_EQ("hello", decryptedText);
+}
